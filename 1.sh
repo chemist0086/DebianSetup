f3f5d6d65332d94bee1197e0f7e3ed9cb9631c79
Xuheng <chemist0086@hotmail.com>
2017-09-01 18:29:48 +0800

    update sh file

--- 1.sh
+++ 1.sh
@@ -1,7 +1,7 @@
 #!/etc/bash
-wget http://ys-k.ys168.com/597833048/h5K5S27655JN63TWRgkw/2.sh
-wget http://ys-k.ys168.com/597833051/VSSkjui5K5S276555NL/myvncserver.sh
-wget http://ys-k.ys168.com/597833051/VSSkjui5K5S276555NM/myvncserver.service
+wget https://raw.githubusercontent.com/chemist0086/DebianSetup/master/2.sh
+wget https://raw.githubusercontent.com/chemist0086/DebianSetup/master/myvncserver.service
+wget https://raw.githubusercontent.com/chemist0086/DebianSetup/master/myvncserver
 sudo apt-get update
 sudo apt-get install -y expect
 sudo apt-get install -y xfce4 tightvncserver
@@ -14,7 +14,7 @@ cp libflashplayer.so  ~/.mozilla/plugins/
 expect 2.sh
 
 vncserver -kill :1
-sudo mv myvncserver.sh /usr/local/bin/myvncserver
+sudo mv myvncserver /usr/local/bin/myvncserver
 sudo chmod +x /usr/local/bin/myvncserver
 sudo mv myvncserver.service /lib/systemd/system/myvncserver.service
 sudo systemctl daemon-reload
